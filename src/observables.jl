37afffd950066ff8075c83328dd4b8c925813318
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-08-08 11:45:44 +0200

    Fixed observables calculation

--- src/observables.jl
+++ src/observables.jl
@@ -3,23 +3,25 @@ using SimpleFields
 
 import Base: call
 
+call(obs::AbstractObservable, Ψ, i, τ, field::Field) = (obs.v[i] = value(obs, Ψ, i, τ, field))
+
 type TimeObservable <: AbstractObservable
     v::Vector{Float64}
     TimeObservable(N::Int) = new(Vector{Float64}(N))
 end
-call(obs::TimeObservable, Ψ, i, τ, field::Field) = (obs.v[i] = (i-1)*τ)
+value(obs::TimeObservable, Ψ, i, τ, field::Field) = (i-1)*τ
 
 type NormObservable <: AbstractObservable
     v::Vector{Float64}
     NormObservable(N::Int) = new(Vector{Float64}(N))
 end
-call(obs::NormObservable, Ψ, i, τ, field::Field) = vecnorm(Ψ)
+value(obs::NormObservable, Ψ, i, τ, field::Field) = norm(Ψ)
 
 type FieldObservable <: AbstractObservable
     v::Vector{Float64}
     FieldObservable(N::Int) = new(Vector{Float64}(N))
 end
-call(obs::FieldObservable, Ψ, i, τ, field::Field) = field((i-1)*τ)
+value(obs::FieldObservable, Ψ, i, τ, field::Field) = field((i-1)*τ/field.T)
 
 const observable_types = Dict(:time => TimeObservable,
                               :norm => NormObservable,
