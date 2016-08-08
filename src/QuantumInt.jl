a77425140be310809c6895b867cf15185387e903
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-08-08 10:47:04 +0200

    Added some observables

--- src/QuantumInt.jl
+++ src/QuantumInt.jl
@@ -2,6 +2,7 @@ module QuantumInt
 
 include("operators.jl")
 include("hamiltonian.jl")
+include("observables.jl")
 include("calc.jl")
 
 end # module
--- src/calc.jl
+++ src/calc.jl
@@ -8,11 +8,11 @@ function calc{M<:AbstractMatrix{Float64}}(observe::Function,
                                           E::Vector{Vector{Float64}},
                                           V::Vector{M},
                                           D::Operator,
-                                          field::Field, ndt::Integer,
-                                          cutoff::Real = 0;
+                                          field::Field, ndt::Integer;
+                                          cutoff::Real = 0,
                                           magnus_kwargs...)
     npartial = length(E)
-    H₀,D,gst = hamiltonian(E, V, D, cutoff, verbose)
+    H₀,D,gst = hamiltonian(E, V, D, cutoff)
 
     ψ₀ = zeros(Complex128, size(H₀,1))
     ψ₀[gst] = 1.0
@@ -28,3 +28,42 @@ function calc{M<:AbstractMatrix{Float64}}(observe::Function,
 
     diag(H₀), Ψ
 end
+
+calc{M<:AbstractMatrix{Float64}}(E::Vector{Vector{Float64}},
+                                 V::Vector{M},
+                                 D::Operator,
+                                 field::Field, ndt::Integer;
+                                 kwargs...) =
+                                     calc((Ψ,i,τ) -> (),
+                                          E, V, D,
+                                          field, ndt;
+                                          kwargs...)
+
+function calc{M<:AbstractMatrix{Float64}}(E::Vector{Vector{Float64}},
+                                          V::Vector{M},
+                                          D::Operator,
+                                          field::Field, ndt::Integer,
+                                          observables::Vector{Symbol},
+                                          extra_observables::Function = (Ψ,i,τ,field) -> ();
+                                          kwargs...)
+
+    N = ceil(Int, ndt*field.tmax)
+
+    obss = []
+    for o in observables
+        o in keys(observable_types) || error("Unknown observable, $(t)")
+        push!(obss, (o,observable_types[o](N)))
+    end
+
+    E, Ψ = calc(E, V, D,
+                field, ndt;
+                kwargs...) do Ψ,i,τ
+                    for o in obss
+                        o[2](Ψ,i,τ,field)
+                    end
+                    extra_observables(Ψ,i,τ,field)
+                end
+    E, Ψ, obss
+end
+
+export calc
